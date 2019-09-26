6ae9dfcc77287ab56478748a9a94c1ee92a985da
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-09-25 22:17:37 -0700

    Comment out juce_dsp SIMD code for Linux

--- modules/juce_dsp/containers/juce_SIMDRegister_Impl.h
+++ modules/juce_dsp/containers/juce_SIMDRegister_Impl.h
@@ -169,9 +169,10 @@ struct CmplxSIMDOps<std::complex<Scalar>>
 } // namespace dsp
 
 // Extend some common used global functions to SIMDRegister types
+#if ! JUCE_LINUX
 template <typename Type>
 inline dsp::SIMDRegister<Type> JUCE_VECTOR_CALLTYPE jmin (dsp::SIMDRegister<Type> a, dsp::SIMDRegister<Type> b) { return dsp::SIMDRegister<Type>::min (a, b); }
 template <typename Type>
 inline dsp::SIMDRegister<Type> JUCE_VECTOR_CALLTYPE jmax (dsp::SIMDRegister<Type> a, dsp::SIMDRegister<Type> b) { return dsp::SIMDRegister<Type>::max (a, b); }
-
+#endif
 } // namespace juce
