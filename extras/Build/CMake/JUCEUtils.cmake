44878cf10a9319aa0eea95ef59d4791048af50ee
jatin <jatinchowdhury18@gmail.com>
2023-01-09 09:40:54 -0800

    Link juce_lv2_helper to pthread

--- extras/Build/CMake/JUCEUtils.cmake
+++ extras/Build/CMake/JUCEUtils.cmake
@@ -845,7 +845,9 @@ function(_juce_add_lv2_manifest_helper_target)
     add_executable(juce::juce_lv2_helper ALIAS juce_lv2_helper)
     target_compile_features(juce_lv2_helper PRIVATE cxx_std_17)
     set_target_properties(juce_lv2_helper PROPERTIES BUILD_WITH_INSTALL_RPATH ON)
-    target_link_libraries(juce_lv2_helper PRIVATE ${CMAKE_DL_LIBS})
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+    find_package(Threads REQUIRED)
+    target_link_libraries(juce_lv2_helper PRIVATE ${CMAKE_DL_LIBS} Threads::Threads)
 endfunction()
 
 # ==================================================================================================
